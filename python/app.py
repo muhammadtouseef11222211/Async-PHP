668a826f15fd1f4e5b67163295f4d1304c5f31bc
Muhammad Touseef <200~muhammadtouseef11222211@gmail.com>
2023-07-30 01:21:21 +0500

    hi

---
+++ python/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route and a function to handle the route
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# Run the application if this script is executed directly
+if __name__ == '__main__':
+    app.run(debug=True)
