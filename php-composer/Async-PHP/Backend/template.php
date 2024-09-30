90cf6f45086f3e5023c31410a70b1c9b74e58910
John Doe <johndoe@example.com>
2024-09-30 12:08:37 +0500

    added

--- php-composer/Async-PHP/Backend/template.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-echo "hellow from AsyncPHP";
-
-
-
-?>
--- php-composer/async_check_sleep/lib/macro/macro_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-function db_connect($host, $user, $password, $dbname, $port = 3306) {
-    return (new mysqli($host, $user, $password, $dbname, $port))->connect_error ? die("Connection failed: " . (new mysqli($host, $user, $password, $dbname, $port))->connect_error) : (new mysqli($host, $user, $password, $dbname, $port));
-}
-
-?>
